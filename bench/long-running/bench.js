99f0b09c3f7a686fc3d59f0a5d8ebc8ae2027ea8
Adrian Toncean <adrianton3@gmail.com>
2016-06-12 17:10:19 +0200

    Add benchmark scripts

---
+++ bench/long-running/bench.js
@@ -0,0 +1,73 @@
+'use strict'
+
+const fs = require('fs')
+
+const { execSync } = require('child_process')
+
+function generateProduct (arrays) {
+	const solutions = []
+
+	function recurse (partial, k) {
+		if (k >= arrays.length) {
+			solutions.push(partial)
+		} else {
+			const array = arrays[k]
+
+			array.forEach((entry) => {
+				recurse([...partial, entry], k + 1)
+			})
+		}
+	}
+
+	recurse([], 0)
+
+	return solutions
+}
+
+function extractTime (buffer) {
+	const string = buffer.toString()
+	const extract = string.match(/\d+/)[0]
+	return extract / 1e6
+}
+
+function execAverage (command, count) {
+	let sum = 0
+
+	for (let i = 0; i < count; i++) {
+		const buffer = execSync(command)
+		sum += extractTime(buffer)
+	}
+
+	return sum / count
+}
+
+const compilers = [
+	//'--basic',
+	//'--optimizing',
+	'--stacking',
+	'--binary'
+]
+
+const runtimes = [
+	//'--lazy',
+	'--eager'
+]
+
+const scripts = [
+	'count-up.bef',
+	'count-up-extra.bef',
+	'count-down.bef'
+].map((name) => `scripts/${name}`)
+
+const runs = 10
+
+const argSet = generateProduct([compilers, runtimes, scripts])
+
+const results = argSet.map((args) => {
+	const command = `node ../../build/befunjit.node.js --no-input --time ${args.join(' ')}`
+	const time = execAverage(command, runs)
+
+	return { args, time }
+})
+
+fs.writeFileSync('./results.json', JSON.stringify(results))
